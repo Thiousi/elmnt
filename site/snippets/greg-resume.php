86478d8fdbcdb5979c886f6d2c531dbafb7e07f5
Greg Smith <greg@elmnt.com>
2016-04-25 16:54:50 -0700

    updated footer

--- site/snippets/greg-resume.php
+++ site/snippets/greg-resume.php
@@ -1,5 +1,26 @@
 <style>
 a { text-decoration: none; }
+.resume__contact {
+  font-size: 1rem;
+  line-height: 1.25em;
+}
+.resume__content {
+  margin-top: 0;
+}
+.resume__content h3 {
+  margin-top: 2rem;
+  margin-bottom: .25rem;
+}
+.resume__content h3:first-child {
+  margin-top: 0;
+}
+.resume__content h5 {
+  /* margin-top: 2rem; */
+  margin-bottom: .5rem;
+}
+.resume__content ul {
+  margin-left: 1.5rem;
+}
 /*
 .resume__skill {
   width: 100%;
@@ -50,24 +71,34 @@ a { text-decoration: none; }
   <div class="single-col">
 
   <div class="grid">
-  <h1><?php echo $page->title()->html() ?></h1>
 
     <div class="col-1-2">
+      <h2><?php echo $page->title()->html() ?></h2>
+      <!--
       <h2 style="margin-bottom:0;">Greg Smith</h2>
       <p>
       <a href="mailto:greg@elmnt.com">greg@elmnt.com</a><br>
       <a href="https://twitter.com/elmnt" target="_blank">@elmnt</a><br>
       <a href="tel:9492578520">949-257-8520</a>
       </p>
+      -->
     </div>
 
     <div class="col-1-2">
-      <img src="/content/home/elmnt-avatar.png" alt="Greg Smith" style="width:100%;max-width:200px;margin:0 auto;">
+      <!-- <img src="/content/home/elmnt-avatar.png" alt="Greg Smith" style="width:100%;max-width:200px;margin:0 auto;"> -->
+      <h2 style="margin-bottom:0;">Greg Smith</h2>
+      <p class="resume__contact">
+      <a href="http://elmnt.com">elmnt.com</a><br>
+      <a href="mailto:greg@elmnt.com">greg@elmnt.com</a><br>
+      <a href="https://twitter.com/elmnt" target="_blank">@elmnt</a><br>
+      <a href="tel:9492578520">949-257-8520</a><br>
+      Portland, OR
+      </p>
     </div>
 
   </div><!-- /.grid -->
 
-  <div class="grid">
+  <div class="grid resume__content">
     <?php echo $page->text_one()->kirbytext() ?>
 
     <!--
