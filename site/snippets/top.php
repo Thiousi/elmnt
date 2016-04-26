48c6288fd233b98f41a50827dca5825a2d0eb0b0
Greg Smith <greg@elmnt.com>
2016-04-25 17:01:01 -0700

    site description

--- site/snippets/top.php
+++ site/snippets/top.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
+<meta name="description" content="<?php echo $site->description()->html() ?>">
 <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
 <!-- Typekit -->
 <script>
@@ -15,7 +16,6 @@
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
 </script>
-<meta name="description" content="<?php echo $site->description()->html() ?>">
 <?php snippet('favicons') ?>
 <meta name="apple-mobile-web-app-title" content="<?php echo html($site->title()) ?>">
 <link rel="alternate" type="application/rss+xml" href="<?php echo url('feed') ?>" title="Blog Feed">
