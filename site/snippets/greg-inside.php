d60eaa2763006b7c14ec90ccff576ae8718fa212
Greg Smith <greg@elmnt.com>
2016-05-05 09:52:44 -0700

    updated info

--- site/snippets/greg-inside.php
+++ site/snippets/greg-inside.php
@@ -1,8 +1,15 @@
 <style>
-a { text-decoration: none; }
+.resume__links {
+  margin-bottom: 0;
+}
+.resume__links a,
+.resume__contact a { 
+  text-decoration: none; 
+}
+.resume__links,
 .resume__contact {
-  font-size: 1rem;
-  line-height: 1.25em;
+  /* font-size: 1rem; */
+  line-height: 1.35em;
 }
 .resume__content {
   margin-top: 0;
@@ -38,16 +45,16 @@ html {
   <div class="grid">
 
     <div class="col-1-2">
-      <h2><?php echo $page->title()->html() ?></h2>
+      <h2 style="margin-bottom: .5rem;"><?php echo $page->title()->html() ?></h2>
       <?php if( $page->id() == "resume" ): ?>
-        <p style="margin-bottom:.125rem;"><a href="/assets/pdf/greg-smith-resume.pdf" target="_blank">View/Download PDF</a></p>
-        <p><a href="https://docs.google.com/document/d/1xJpW8tEVrJCJYg2WY6vzwIKRsIocnexsb2sUTUEVoxk/edit?usp=sharing" target="_blank">View in Google Docs</a></p>
+        <p class="resume__links"><a href="/assets/pdf/greg-smith-resume.pdf" target="_blank">View/Download PDF</a></p>
+        <p class="resume__links"><a href="https://docs.google.com/document/d/1xJpW8tEVrJCJYg2WY6vzwIKRsIocnexsb2sUTUEVoxk/edit?usp=sharing" target="_blank">View in Google Docs</a></p>
       <?php endif ?>
     </div>
 
     <div class="col-1-2">
       <?php if( $page->id() == "resume" ): ?>
-      <h2 style="margin-bottom:0;">Greg Smith</h2>
+      <h2 style="margin-bottom: .5rem;">Greg Smith</h2>
       <p class="resume__contact">
       <a href="http://elmnt.com">elmnt.com</a><br>
       <a href="mailto:greg@elmnt.com">greg@elmnt.com</a><br>
